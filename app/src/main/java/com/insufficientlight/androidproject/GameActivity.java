1d223479f8cdb8130d3acdea6e557292cb9acbe8
wgrim <42billys@gmail.com>
2019-05-02 09:38:55 -0400

    Slightly  Changed things

--- app/src/main/java/com/insufficientlight/androidproject/GameActivity.java
+++ app/src/main/java/com/insufficientlight/androidproject/GameActivity.java
@@ -8,6 +8,7 @@ public class GameActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //yeet
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
