0e58eb23739a5579b776ae491bd783e634e53e1b
Billyjim1 <42billys@gmail.com>
2019-05-11 23:26:36 -0400

    Worked on the Combat System

--- app/src/main/java/com/insufficientlight/androidproject/StandardSkirmish.java
+++ app/src/main/java/com/insufficientlight/androidproject/StandardSkirmish.java
@@ -5,11 +5,25 @@ public class StandardSkirmish extends Skirmish
     //Basic Skirmish
     Army Attacker = null;
     Army Defender = null;
+    String P1Form = null;
+    String P2Form = null;
+    String P1Cav = null;
+    String P2Cav = null;
+    String P1Tac = null;
+    String P2Tac = null;
 
-    public StandardSkirmish(Army Defender, Army Attacker)
+
+
+    public StandardSkirmish(Army Defender, Army Attacker, String P1Form, String P2Form, String P1Cav, String P2Cav, String P1Tac, String P2Tac)
     {
        this.Defender = Defender;
        this.Attacker = Attacker;
+       this.P1Form = P1Form;
+       this.P2Form = P2Form;
+       this.P1Cav = P1Cav;
+       this.P2Cav = P2Cav;
+       this.P1Tac = P1Tac;
+       this.P2Tac = P2Tac;
     }
 
 
