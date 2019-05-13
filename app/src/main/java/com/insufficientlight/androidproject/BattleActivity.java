8ffcd682ea068148209b48abf0dd851fd93f0521
wgrim <42billys@gmail.com>
2019-05-13 10:43:37 -0400

    Worked Some more on the combat mechanics

--- app/src/main/java/com/insufficientlight/androidproject/BattleActivity.java
+++ app/src/main/java/com/insufficientlight/androidproject/BattleActivity.java
@@ -2,6 +2,7 @@ package com.insufficientlight.androidproject;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class BattleActivity extends GameActivity
@@ -20,15 +21,37 @@ public class BattleActivity extends GameActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
+
+        NumberPicker Formation = (NumberPicker) findViewById(R.id.form);
+        NumberPicker ArcherTactics = (NumberPicker) findViewById(R.id.arch);
+        NumberPicker CavalryTactics = (NumberPicker) findViewById(R.id.cav);
+
         Title = findViewById(R.id.titleView);
         Army1 = findViewById(R.id.attackerView);
         Army2 = findViewById(R.id.defenderView);
         Terrain = findViewById(R.id.terrainView);
 
+        final String[] Formations = {"Shield Wall","Phalanx", "Turtle Formation"};
+        final String[] ArcTac = {"Careful Volleys", "Full Volleys", "Protect Flanks"};
+        final String[] CavTac = {"Charge Front Lines","Flanking Operation", "Hold Cavalry"};
+
+        Formation.setMinValue(0);
+        ArcherTactics.setMinValue(0);
+        CavalryTactics.setMinValue(0);
+
+        Formation.setDisplayedValues(Formations);
+        ArcherTactics.setDisplayedValues(ArcTac);
+        CavalryTactics.setDisplayedValues(CavTac);
+
+        Formation.setWrapSelectorWheel(true);
+        ArcherTactics.setWrapSelectorWheel(true);
+        CavalryTactics.setWrapSelectorWheel(true);
+
         Terrain.setText(battle.getTerrain().getTerrainType());
-        Title.setText("The battle of "+battle.getLocation()+"!");
+        Title.setText("The battle of "+ battle.getLocation()+"!");
         Army1.setText("Attacker: " + battle.attacker.armyName);
         Army2.setText("Defender: " + battle.defender.armyName);
-
+        Army1.append("\n Infantry: " + battle.attacker.numInf + "\n Archers: " + battle.attacker.numArc + "\n Cavalry :" + battle.attacker.numCav +"\n Siege Weapons: " + battle.attacker.numSie);
+        Army2.append("\n Infantry: " + battle.defender.numInf + "\n Archers: " + battle.defender.numArc + "\n Cavalry :" + battle.defender.numCav +"\n Siege Weapons: " + battle.defender.numSie);
     }
 }
